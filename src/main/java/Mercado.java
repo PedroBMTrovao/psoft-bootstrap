2985fa903ee828440ad64f4fdb2032aea656901b
PedroBMTrovao <pedro.brito.de.melo.trovao@ccc.ufcg.edu.br>
2026-09-23 19:28:49 -0300

    Update Mercado.java 1

--- src/main/java/Mercado.java
+++ src/main/java/Mercado.java
@@ -1,1 +1,42 @@
+// Os tipos de alguns atributos e argumentos foram substituídos
+import java.util.ArrayList;
 
+public class Mercado {
+  private ArrayList<Venda> vendas;
+  private ArrayList<Cliente> clientes;
+  private ArrayList<Produto> produtos;
+  private int noVenda;
+  public Mercado() {
+    this.vendas = new ArrayList<>();
+    this.clientes = new ArrayList<>();
+    this.produtos = new ArrayList<>();
+    this.noVenda = 1;
+  }
+  public void createCliente(String name, boolean status) {
+    for (Cliente client : clientes) {
+      if (name.equals(client.getName())) {
+        throw new IllegalArgumentException("Já existe");
+      }
+    }
+    cliente = new Cliente(name, status);
+    clientes.add(cliente);
+  }
+  public boolean changeUserStatus(String name) {
+    for (Cliente client : clientes) {
+      if (name.equals(client.getName())) {
+        client.changeStatus();
+        break;
+      }
+    }
+    throw new IllegalArgumentException("Não existe");
+  }
+  public void addProduto(String name, String price) {
+    for (Produto prod : produtos) {
+      if (name.equals(prod.getName())) {
+        throw new IllegalArgumentException("Já existe");
+      }
+    }
+    produto = new Produto(name, price);
+    produtos.add(produto);
+  }
+}
