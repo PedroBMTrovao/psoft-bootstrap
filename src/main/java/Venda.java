86e38e7bacb430967a4eaaacbfb818c4d7b1fde2
PedroBMTrovao <pedro.brito.de.melo.trovao@ccc.ufcg.edu.br>
2026-09-23 20:10:10 -0300

    Update Venda.java

--- src/main/java/Venda.java
+++ src/main/java/Venda.java
@@ -1,1 +1,20 @@
+// Os tipos de alguns atributos e argumentos foram substituídos
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
 
+public class Venda {
+  private int numero;
+  private Cliente cliente;
+  private boolean pagamento;
+  private ArrayList<ItemVenda> itens;
+  public Venda(int number, Cliente client) {
+    this.numero = number;
+    this.cliente = client;
+    this.pagamento = false;
+    this.itens = new ArrayList<>();
+  }
+  public void addItem(Produto product, int quant) {
+    
+  }
+}
