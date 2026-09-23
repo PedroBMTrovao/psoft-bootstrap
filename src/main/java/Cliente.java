0f62caf6db1a981f7384a607cc2f6bad60b8a1b5
PedroBMTrovao <pedro.brito.de.melo.trovao@ccc.ufcg.edu.br>
2026-09-23 18:46:46 -0300

    Update Cliente.java

--- src/main/java/Cliente.java
+++ src/main/java/Cliente.java
@@ -1,1 +1,18 @@
-
+public class Cliente {
+  private String nome;
+  private boolean status;
+  public Cliente(String name, boolean status) {
+    this.nome = name;
+    this.status = status;
+  }
+  public String getName() {
+    return nome;
+  }
+  public boolean isPremium() {
+    return status;
+  }
+  public boolean changeStatus() {
+    this.status = !this.status;
+    return this.status;
+  }
+}
