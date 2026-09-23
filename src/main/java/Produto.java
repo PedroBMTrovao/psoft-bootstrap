83be8dcfa986efda45bbe4ac0858873fed2ee7f5
PedroBMTrovao <pedro.brito.de.melo.trovao@ccc.ufcg.edu.br>
2026-09-23 18:35:25 -0300

    Update Produto.java

--- src/main/java/Produto.java
+++ src/main/java/Produto.java
@@ -1,1 +1,17 @@
+// Os tipos de alguns atributos foram substituídos
+import java.math.BigDecimal;
 
+public class Produto {
+  private String nome;
+  private BigDecimal preco;
+  public Produto(String name, String price) {
+      this.nome = name;
+      this.preco = new BigDecimal(price);
+  }
+  public BigDecimal getPrice() {
+      return preco;
+  }
+  public String getName() {
+      return nome;
+  }
+}
